fbd47717631c619f332e58f67a9881374fcbb605
XavarsCode <138240303+XavarsCode@users.noreply.github.com>
2025-08-04 11:13:47 +0200

    Delete Shindo-Life-RP-Bot-v1 directory

--- Shindo-Life-RP-Bot-v1/utils/keepAlive.js
+++
@@ -1,8 +0,0 @@
-// Fonction pour maintenir le bot en vie sur Replit
-function keepAlive() {
-    setInterval(() => {
-        console.log('🤖 Bot toujours actif...');
-    }, 5 * 60 * 1000); // Toutes les 5 minutes
-}
-
-module.exports = { keepAlive };
